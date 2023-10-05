4a8642e57e6c8886bcf67e650729e23b86bf27d7
thobzabuyi <37335706+thobzabuyi@users.noreply.github.com>
2023-10-05 10:11:18 +0200

    add sql query file via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,19 @@
+create table Brand (Id int primary key identity(1,1), BrandName varchar(50))
+
+insert into Brand (BrandName) values ('Visa')
+insert into Brand (BrandName) values ('TymeBank')
+insert into Brand (BrandName) values ('Distell')
+insert into Brand (BrandName) values ('Spotify')
+insert into Brand (BrandName) values ('Microsoft')
+insert into Brand (BrandName) values ('Engen')
+insert into Brand (BrandName) values ('Nike')
+insert into Brand (BrandName) values ('Wesgro')
+insert into Brand (BrandName) values ('Multichoice')
+insert into Brand (BrandName) values ('PickNPay')
+insert into Brand (BrandName) values ('Liquid')
+insert into Brand (BrandName) values ('Tfg')
+insert into Brand (BrandName) values ('Sanlam')
+insert into Brand (BrandName) values ('Santam')
+insert into Brand (BrandName) values ('Bcc')
+
+
